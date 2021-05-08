4c30ee2fa9a47f0fe827c29c6315387c4c55ef21
Ethan Lam <ethan.lam@mail.utoronto.ca>
2021-05-08 17:18:46 -0400

    add firebase file with info

---
+++ components/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase"
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCSuLLXopWulAnWrr1Y5-xQgtNhqfcKTDA",
+    authDomain: "lectern-d16e5.firebaseapp.com",
+    projectId: "lectern-d16e5",
+    storageBucket: "lectern-d16e5.appspot.com",
+    messagingSenderId: "217028493707",
+    appId: "1:217028493707:web:bfed67283292a9a4912f8e",
+    measurementId: "G-44JVC6P1JJ"
+};
